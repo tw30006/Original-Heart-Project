f1de355678fb4d3edcc3920a84bdda9dd392fbc2
tw30006 <original.heart.2016@gmail.com>
2024-02-27 10:36:53 +0800

    修改base

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
+  // base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
+  base: '/Origin-Heart-Project/',
   plugins: [
     vue(),
     eslintPlugin({
